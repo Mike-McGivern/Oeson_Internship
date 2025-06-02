60733c47fb10534c761834c35c801abf7aa09160
Michael P McGivern II <mpm118@pitt.edu>
2025-06-02 11:30:26 -0400

    Update 202_Happy_Number.py

--- AssignedLeetcode/202_Happy_Number.py
+++ AssignedLeetcode/202_Happy_Number.py
@@ -5,5 +5,22 @@ class Solution:
             seen.add(n)
             n = sum(int(digit) ** 2 for digit in str(n))
 
-        return n == 1    
-    
+        return n == 1  
+
+
+
+#def recIsHappy(self, n: int) -> bool:
+  #  n = list(str(n))
+  #  toReturn = 0
+  #  for digit in range(len(n)):
+ #       toReturn = toReturn + int(n[digit]) ** 2
+#    if toReturn == 1:
+#        return True
+#    else:
+#        return Solution.isHappy(self, toReturn)
+#def isHappy(self, n: int) -> bool:
+#    sys.setrecursionlimit(1000)
+#    try:
+ #       return Solution.recIsHappy(self, n)
+ #   except RecursionError:
+  #      return False
